03e94a2bae891639914c809e3ca4f86d3eeffe2a
shubhamchavan123 <chavanshubham936@gmail.com>
2024-12-26 14:49:09 +0530

    push first

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+//schema
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "user name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      required: [true, "phone number is require"],
+    },
+    
+  },
+  { timestamps: true }
+);
+
+//export
+module.exports = mongoose.model("User", userSchema);
